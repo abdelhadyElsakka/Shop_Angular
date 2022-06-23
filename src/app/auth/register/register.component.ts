391f3a92024e4b6b4fe037abe7c3b367e9515ce1
Abdelhady El-Sakka <58234746+abdelhadyElsakka@users.noreply.github.com>
2022-06-23 10:48:56 +0200

    Add files via upload

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from '../confirmed.validator';
 
 @Component({
   selector: 'app-register',
@@ -8,26 +9,52 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class RegisterComponent implements OnInit {
 
-  strongPasswordPattern:RegExp = new RegExp("^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#\$%\^&\*])(?=.{8,})");
+  passwordPattern:RegExp = new RegExp("^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#\\$%\\^&\\*])(?=.{8,})");
   emailPattern:RegExp = new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
-  userNamePattern:RegExp = new RegExp("/^\S*$/");
+  userNamePattern:RegExp = new RegExp("^[^\\s]+(\\s+[^\\s]+)*$");
 
   signUp:FormGroup = this.fb.group ({
     name: ["",[Validators.required]],
     email: ["",[Validators.required,Validators.email,Validators.pattern(this.emailPattern)]],
     userName: ["",[Validators.required,Validators.pattern(this.userNamePattern)]],
-    password: ["",[Validators.required,Validators.minLength(8),Validators.pattern(this.strongPasswordPattern)]],
-    confirmPassword: ["",[Validators.required]],
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(this.passwordPattern),
+    ]),
+    addresses: this.fb.array([]),
+    confirmPassword: new FormControl('', [Validators.required])
+  },
 
-  })
+  CustomValidators.mustMatch('password', 'confirmPassword')
+  );
 
   showError:boolean=false;
+  
+  constructor(private fb:FormBuilder) {}
 
-  constructor(private fb:FormBuilder) { }
+  get addresses() {
+    return this.signUp.controls["addresses"] as FormArray;
+  }
+
+  addAddress() {
+    const addressForm = this.fb.group({
+        address: ['', Validators.required],
+        street: ['', Validators.required],
+        country: ['', Validators.required],
+        city: ['', Validators.required]
+    });
+  
+    this.addresses.push(addressForm);
+  }
+
+  deleteAddress(addressIndex: number) {
+    this.addresses.removeAt(addressIndex);
+  }
 
   ngOnInit(): void {
   }
-
+  
   get signUpValidation(){
     return this.signUp.controls;
   }
